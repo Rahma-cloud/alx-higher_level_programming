cfb447eae32174d8eacde4d1179e209ea7bb9fb0
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-06-06 17:40:17 -0700

    task11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def pow(a, b):
-    return a ^ b
+    if b >= 0:
+        result = 1
+        for _ in range(b):
+            result *= a
+        return result
+    else:
+        result = 1
+        for _ in range(abs(b)):
+            result /= a
+            return result
