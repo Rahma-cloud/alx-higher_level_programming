b30bdfb49365195cbc158fabf116f1945f04249d
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-06-10 08:03:35 -0700

    task1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #/usr/bin/python
 def element_at(my_list, idx):
-    if idx < 0 and idx > len(my_list):
-        print("None")
+    if idx > 0 and idx < len(my_list):
+        return my_list[idx]
     else:
-        print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+        return None
