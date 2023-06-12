c0129869b47043fd0f57f40ccab5873128ce1388
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-06-12 15:44:44 -0700

    task8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     my_length = len(sentence)
     first_c = sentence[0]
-    if sentence == "":
+    if sentence == " ":
         return my_length, None
     return my_length, first_c
