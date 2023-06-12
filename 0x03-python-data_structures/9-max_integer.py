677c324fb17ac45fb67a1c3142aff311da6417e2
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-06-12 15:54:42 -0700

    task9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0
+    if my_list == "":
+        return None
+    for i in my_list:
+        if i > my_list[i]:
+            return i
