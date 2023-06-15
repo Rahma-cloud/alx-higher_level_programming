3cd507a226917c0374b8f2fadd5257e45dd8adf9
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-06-14 18:40:13 -0700

    task0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    powerMatrix = []
+    for row in matrix:
+        powerMatrix.append(list(map(lambda x:x**2, row))))
+    return powerMatrix
