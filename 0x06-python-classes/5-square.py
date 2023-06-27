5c8fb1cb83588b3f95fb0a6f3b05c2ca11171298
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-06-27 14:51:43 -0700

    task5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,15 +3,19 @@
 
 
 class Square:
+    """ This is a square"""
     def __init__(self, size=0):
+        """This is to initialize"""
         self.size = size
 
     @property
     def size(self):
+        """to get"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """to set"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -19,9 +23,11 @@ class Square:
         self.__size = value
 
     def area(self):
+        """The area function"""
         return self.__size ** 2
 
     def my_print(self):
+        """to print"""
         if self.__size == 0:
             print()
         else:
