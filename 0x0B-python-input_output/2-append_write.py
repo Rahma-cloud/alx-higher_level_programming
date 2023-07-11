997e50ab3e5355e6f7d21fe85ba15587e50537d6
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-07-11 10:30:09 -0700

    task2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+write to a file
+"""
+
+
+def append_write(filename="", text=""):
+    """my module"""
+    with open(filename, "a" encoding="UTF-8") as fp:
+        return fp.write(text)
