3f5ac95d6ae97950e82d5fcfde7c18296d495415
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-07-11 10:35:18 -0700

    task2 and 3

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-write to a filr
+write to a file
 """
 
 
 def write_file(filename="", text=""):
     """my module"""
-    with open(filename, "w" encoding="UTF-8") as fp:
+    with open(filename, "w", encoding="UTF-8") as fp:
         return fp.write(text)
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,5 +6,5 @@ write to a file
 
 def append_write(filename="", text=""):
     """my module"""
-    with open(filename, "a" encoding="UTF-8") as fp:
+    with open(filename, "a", encoding="UTF-8") as fp:
         return fp.write(text)
