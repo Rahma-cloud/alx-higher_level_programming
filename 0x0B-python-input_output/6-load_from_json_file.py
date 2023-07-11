f2f4021781af1f209224c3dbb401461224ef14d3
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-07-11 13:53:37 -0700

    task6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -4,9 +4,10 @@ my task
 """
 
 
+import json
+
+
 def load_from_json_file(filename):
     """my module"""
     with open(filename, "r", encoding="UTF-8") as fp:
-        json_data = fp.read()
-        obj = json.loads(json_data)
-        return obj
+        return json.loads(fp)
