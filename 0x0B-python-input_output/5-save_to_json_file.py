65e1698b44ef5f4e859ebbd134f8f69db0e27ab2
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-07-11 11:00:31 -0700

    task5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -9,5 +9,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """my module"""
-    with open(filename, "r", encoding="UTF-8")as fp:
+    with open(filename, "rw", encoding="UTF-8")as fp:
         json.dump(my_obj, fp)
