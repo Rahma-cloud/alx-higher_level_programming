c0924e9cecb9714d813bf4d1e0a70cea4c8211f9
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-08-16 14:03:40 -0700

    tasks

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,7 +1,4 @@
 -- Task5
 -- Creating a table in sql
-CREATE TABLE IF NOT EXISTS `unique_id` (
-	`id`INT, DEFAULT 1,
-	UNIQUE(`id`),
-	`name` VARCHAR(256)
-);
+CREATE TABLE IF NOT EXISTS `unique_id` (`id`INT, DEFAULT 1,
+UNIQUE(`id`), `name` VARCHAR(256));
