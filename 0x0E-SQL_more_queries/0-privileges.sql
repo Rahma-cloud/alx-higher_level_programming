ab674eef21c2284619de65c28a5ba6f345e8b523
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-08-16 08:54:23 -0700

    tasks

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,4 @@
 --Task 0
 -- showing all privileges
-SHOW GRANTS FOR
-'user_0d_1'@'localhost';
-SHOW GRANTS FOR
-'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
