c3a408555dec010a9ff2e84056c3c4e5984ea499
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-08-16 13:55:08 -0700

    tasks

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,9 +2,10 @@
 -- creating database and table
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 USE `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `states` (`id`INT
-	AUTO_INCREMENT NOT NULL PRIMARY KEY, `state_id` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `cities` (
+	`id`INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	`state_id` INT NOT NULL,
 	FOREIGN KEY (`state_id`) REFERENCES `states`
 (`id`),
 	`name` VARCHAR(256) NOT NULL
-	);
+);
