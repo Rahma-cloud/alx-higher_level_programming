2ada66fc85cb753bd91792f0246ee94d1e70d08c
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-08-16 10:19:05 -0700

    tasks

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- Task 6
 -- creating database and table
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `states` (`id`INT, `name`VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS `states` (`id`INT PRIMARY KEY, `name`VARCHAR(256) NOT NULL);
