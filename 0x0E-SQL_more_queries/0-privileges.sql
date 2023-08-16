c7965d0a688d063ce2f4563915c042930c291de0
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-08-16 08:15:55 -0700

    tasks

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+--Task 0
+-- showing all privileges
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_od_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Task 1
+-- create mysql
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
