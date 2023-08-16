fc07e7e37a91da575d50c8eb9b2cd26aad9c376e
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-08-16 13:49:59 -0700

    tasks

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,6 +5,6 @@ USE `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `states` (`id`INT
 	AUTO_INCREMENT NOT NULL PRIMARY KEY, `state_id` INT NOT NULL,
 	FOREIGN KEY (`state_id`) REFERENCES `states`
-	(`id`),
+(`id`),
 	`name` VARCHAR(256) NOT NULL
 	);
