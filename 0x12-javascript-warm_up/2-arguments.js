ed190e4f6ab5b86c365ac831c51897b32c876580
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-11 20:37:22 +0100

    tasks

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argv = process.argv;
+
+if (argv === 0) {
+  console.log('No argument');
+} else if (argv === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
+}
