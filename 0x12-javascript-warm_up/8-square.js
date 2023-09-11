d60983b3e480bd8a8c510a587ff293ef0bb0c23a
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-11 22:48:12 +0100

    tasks

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,9 @@ const argv = process.argv;
 
 if (!isNaN(argv[2])) {
   for (let i = 0; i < argv[2]; i++) {
-    console.log('X');
+    for (let j = 0; j < argv[2]; j++) {
+      console.log('X');
+    }
   }
 } else {
   console.log('Missing size');
