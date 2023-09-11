0bba6bf5937140e1d2c98dcc6fbcfe5c636afd02
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-11 21:32:37 +0100

    tasks

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,9 +2,5 @@
 const argv = process.argv;
 
 if (argv.length === 3) {
-  console.log(argv[2] + 'is' + argv[3]);
-} else if (argv.length <= 2) {
-  console.log('c is undefined');
-} else {
-  console.log('undefined is undefined');
+  console.log(argv[2] + ' is ' + argv[3]);
 }
