fc15e620faf7ebd3e6f52b2a0b25762b5756d5f0
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-12 00:12:37 +0100

    tasks

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,10 +2,10 @@
 const argv = process.argv;
 
 function add (a, b) {
-  if (a && b) {
-    console.log(a + b);
-  } else {
+  if (isNaN(a) || isNaN(b)) {
     console.log('NaN');
+  } else {
+    console.log(a + b);
   }
 }
 add(argv[2], argv[3]);
