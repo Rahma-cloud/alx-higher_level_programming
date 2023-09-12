f732da5b2afacd35da8bd797a6af44aa98012590
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-12 17:32:54 +0100

    tasks

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0) {
-      return {};
+      return Return {};
     } else {
       this.width = w;
       this.height = h;
---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return Rectangle {};
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+    print () {
+      console.log('X');
+    }
+  }
+};
