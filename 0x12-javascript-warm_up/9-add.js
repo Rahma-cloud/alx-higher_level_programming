116391079ee3f1867bbd1f0e0918551f9d22b1ec
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-12 08:51:34 +0100

    tasks

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,7 +5,7 @@ function add (a, b) {
   if (isNaN(a) || isNaN(b)) {
     console.log('NaN');
   } else {
-    console.log(a + b);
+    console.log(Number(a) + Number(b));
   }
 }
 add(argv[2], argv[3]);
