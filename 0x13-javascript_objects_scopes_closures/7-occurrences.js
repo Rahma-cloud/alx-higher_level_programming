9a69bacd7ef5050088b21484e161c36441a6be66
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-12 20:38:40 +0100

    tasks

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,7 +3,7 @@ module.exports = exports.nbOccurences = function (list, searchElement) {
   let count = 0;
   for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
-      count++;i
+      count++;
     }
   }
   console.log(count);
