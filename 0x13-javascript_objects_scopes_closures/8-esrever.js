a453d5b3545da8fa55ac24a86dcc0708e7a17b7b
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-12 20:56:09 +0100

    tasks

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+module.exports = exports.esrever = function (list) {
+  const o = [];
+  const r = [];
+  for (let i = o.length - 1; i >= 0; i--) {
+    r.push(o[i]);
+  }
+  console.log(r);
+};
