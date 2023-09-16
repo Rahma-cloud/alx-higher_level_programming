7e964e6dacdd0c62f057a5323926427f3281679b
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-16 16:54:28 +0100

    tasks

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,21 +12,21 @@ if __name__ == "__main__":
     if len(argv) == 5:
         username = argv[1]
         password = argv[2]
-	database = argv[3]
-	state_name = argv[4]
-	conn = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database,
-            charset="utf8",
-        )
-        cur = conn.cursor()
-        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-	cur.execute(query, (state_name,))
-        query_rows = cur.fetchall()
-        for row in query_rows:
-            print(row)
-        cur.close()
-        conn.close()
+    database = argv[3]
+    state_name = argv[4]
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8",
+    )
+    cur = conn.cursor()
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(query, (state_name,))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
