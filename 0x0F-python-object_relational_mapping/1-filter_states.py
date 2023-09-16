5789b413f79419013f783eb3e24962f0a5f3ed7b
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-16 15:12:51 +0100

    tasks

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         )
         cur = conn.cursor()
         cur.execute(
-            "SELECT * FROM states WHERE name LIKE BINARY \
+            "SELECT * FROM states WHERE NAME LIKE BINARY \
 		'N%' ORDER BY id ASC"
         )
         query_rows = cur.fetchall()
