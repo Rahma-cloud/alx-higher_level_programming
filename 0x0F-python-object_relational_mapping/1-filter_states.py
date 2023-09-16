1d8f465ad21d79de851ccbfdd745d76f542616f6
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-16 11:56:17 +0100

    tasks

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,12 +1,30 @@
 #!/usr/bin/python3
 """
-Task2 module
+Task0 module
 """
 
 
-from sys import argv
 import MySQLdb
+from sys import argv
 
 
-if __name__ == "--main__":
-
+if __name__ == "__main__":
+    if len(argv) == 4:
+        username = argv[1]
+        password = argv[2]
+        database = argv[3]
+        conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database,
+            charset="utf8",
+        )
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        conn.close()
