b47d3e01d2d41ff4f76fd7e476843ac455155a7e
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-17 16:05:40 +0100

    tasks

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,5 +4,16 @@ Task 6 module
 """
 
 
-from sys import argv
-import MySQLdb 
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
