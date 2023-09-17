fe936bc866dd5274337ad090144b6a3728212aa2
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-17 20:05:26 +0100

    tasks

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -6,7 +6,7 @@ Task 9 module
 
 import sys
 from model_state import Base, State
-
+from sqlalchemy import Column, Integer, String
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
