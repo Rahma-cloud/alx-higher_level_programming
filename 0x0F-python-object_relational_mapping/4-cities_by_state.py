571bbe11d6a62ba954bd98632053687ee26f6049
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-17 09:00:30 +0100

    tasks

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Task2 module for mysql task
+"""
+
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    if len(argv) == 4:
+        username = argv[1]
+        password = argv[2]
+        database = argv[3]
+        conn = MySQLdb.connect(
+          host="localhost",
+          port=3306,
+          user=username,
+          passwd=password,
+          db=database,
+          charset="utf8",
+        )
+        cur = conn.cursor()
+        query = "SELECT * FROM cities ORDER BY id ASC"
+        cur.execute(query)
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        conn.close()
