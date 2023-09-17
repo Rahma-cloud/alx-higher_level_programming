75471991fb5eb52ec9f8d9ace9280345c2f90f86
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-17 10:27:31 +0100

    tasks

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -24,8 +24,8 @@ if __name__ == "__main__":
         )
         cur = conn.cursor()
         query = "SELECT cities.id, cities.name, states.name FROM cities \
-JOIN states ON cities.state_id = states.id WHERE states.name = s% \
-ORDER BY cities.id ASC", (state_name,)
+JOIN states ON cities.state_id = states.id WHERE states.name = '{}' \
+ORDER BY cities.id ASC".format(state_name)
         cur.execute(query)
         query_rows = cur.fetchall()
         for row in query_rows:
