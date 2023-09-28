1ef6a3fc60dac47e95a376696a731892fb754632
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-28 23:28:28 +0100

    tasks

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #task 3
-curl -sIX OPTIONS $1 | grep "Allow:" | cut -d ' ' -f 2-
+curl -sIX OPTIONS "$1" | grep "Allow:" | cut -d ' ' -f 2-
---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,6 @@
+#Task 6
+def find_peak(list_of_integers):
+    low, high = 0, len(arr) - 1
+
+    while low < high:
+      mid = (low + high) // 2
