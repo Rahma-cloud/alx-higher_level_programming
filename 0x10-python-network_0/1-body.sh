52b79e152c6289fef946be99f0371e50e6c800e1
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-28 17:29:09 +0100

    tasks

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Task 1
-curl -s -o - -w "%{http_code}" "$1" | awk 'NR==1{status=$1; next} NR>1 && status==200{print}'
+curl -s -o - -w "%{hhtp_code}" ["$1"] | awk 'NR==1 {status=$1} NR>1 && status==200 {print}'
