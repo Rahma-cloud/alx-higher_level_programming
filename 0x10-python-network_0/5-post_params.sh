8ca85864ab541fa4950424a6b2324ba0e1824c83
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-28 23:01:17 +0100

    tasks

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Task5
-email="test@gmail.com"
-subject="I will always be here for PLD"
-curl -sX POST "$1" -d "email=$email&subject=$subject"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
