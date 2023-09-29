e455416826ebcdcd4ca841cbc57771bfa284ff68
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-29 17:18:02 +0100

    pythonNetwork1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-import requests
-import sys
-payload = {'email': sys.argv[2]}
-response = requests.post(sys.argv[1], data=payload)
-print(response.text)
+"""urllib module"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    payload = {'email': sys.argv[2]}
+    response = requests.post(sys.argv[1], data=payload)
+    print(response.text)
