5170ec39616fda7de6ced5bfa21858e664c75897
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-09-29 04:42:22 -0700

    pythonNetwork1

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import requests
+import sys
+if len(sys.argv) == 2:
+    letter = sys.argv[1]
+else:
+    letter = ""
+payload = {'q': letter}
+try:
+    r = requests.post('http://0.0.0.0:5000/search_user', data=payload)
+    r_data = r.json()
+    if r_data:
+        print("[{}] {}".format(r_data.get('id'), r_data.get('name')))
+    else:
+        print("No result")
+except ValueError:
+    print("Not a valid JSON")
+except Exception as e:
+    print(f"Error: {e}")
