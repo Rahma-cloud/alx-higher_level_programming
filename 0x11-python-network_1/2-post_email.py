0aa4389e603ef4cd58a5083f45aadcf24b9d922d
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-29 17:11:36 +0100

    pythonNetwork1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""urllib module"""
 import urllib.request
 import sys
-email_encoded = sys.argv[2].replace('@', '%40').replace('.', '%2E')
-data = 'email=' + email_encoded
-with urllib.request.urlopen(
+if __name__ == "__main__":
+    email_encoded = sys.argv[2].replace('@', '%40').replace('.', '%2E')
+    data = 'email=' + email_encoded
+    with urllib.request.urlopen(
         sys.argv[1], data=data.encode('utf-8')
-        ) as response:
-    content = response.read().decode('utf-8')
-    print(content)
+    ) as response:
+        content = response.read().decode('utf-8')
+        print(content)
