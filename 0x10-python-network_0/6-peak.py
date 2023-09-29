bf2de84a035e30b2cf630afcccf14730cedcdc17
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-29 04:51:27 +0100

    tasks

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,6 +2,8 @@
 # Task 6
 def find_peak(list_of_integers):
     # this is a function to find the peak of unsorted array
+    if not list_of_integers:
+        return None
     low, high = 0, len(list_of_integers) - 1
     while low < high:
         mid = (low + high) // 2
