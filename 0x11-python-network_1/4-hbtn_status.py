2a9335c6b40460bc5ed60eb4c4abf81131f8dd22
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-29 11:54:34 +0100

    pythonNetwork1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 import requests
-<<<<<<< HEAD
 r = requests.get('https://alx-intranet.hbtn.io/status')
-print(r.text)
-=======
-r = requests.get('https://alx-intranet.hbtn.io/status', auth=('user', 'pass'))
-r.status_code
->>>>>>> 66e7130f41ba9442c2e8bcacab55d333aa37568f
+content = r.text
+print("Body response:")
+print(f"\t- type: {type(content)}")
+print(f"\t- content: {content}")
