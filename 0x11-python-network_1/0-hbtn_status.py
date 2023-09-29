4897f057def5c295b934ea5f44df04e743556ec0
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-29 09:19:17 +0100

    pythonNetwork2

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 import urllib.request
 with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-    html = response.read()
-    print(html)
+    content = response.read()
+    utf8_content = content.decode('utf-8')
+print("Body response:")
+print(f"    - type: {type(content)}")
+print(f"    - content: {content}")
+print(f"    - utf8 content: {utf8_content}")
