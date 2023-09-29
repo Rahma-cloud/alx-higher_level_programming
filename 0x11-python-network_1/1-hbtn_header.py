68dabd2c2bfa2ba1043068d6238b8242a7b37359
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-29 13:17:33 +0100

    pythonNetwork1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import urllib.request
+"""urllib module"""
 import sys
+"""sys module"""
 with urllib.request.urlopen(sys.argv[1]) as response:
     answer = response.getheader('X-Request-Id')
 print(answer)
