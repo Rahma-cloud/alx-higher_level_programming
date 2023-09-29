3206f7dfd29663830a8e463b50d1ac5d4df36d3e
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-29 10:56:03 +0100

    task6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-# Task 6
+"""Task 6"""
+
+
 def find_peak(list_of_integers):
+    """to finding peak"""
     if not list_of_integers:
         return None
     low, high = 0, len(list_of_integers) - 1
