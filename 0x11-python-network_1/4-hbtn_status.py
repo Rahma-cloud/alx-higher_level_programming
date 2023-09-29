4713a6d9be694421f580a4d523a338d08d81ae7a
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-29 17:15:18 +0100

    pythonNetwork1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-import requests
-r = requests.get('https://alx-intranet.hbtn.io/status')
-content = r.text
-print("Body response:")
-print(f"\t- type: {type(content)}")
-print(f"\t- content: {content}")
+"""urllib module"""
+if __name__ == "__main__":
+    import requests
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    content = r.text
+    print("Body response:")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
