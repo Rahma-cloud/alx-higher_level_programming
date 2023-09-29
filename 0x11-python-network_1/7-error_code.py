29094649372e30a396a32097eab38878adb69045
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-09-29 17:19:30 +0100

    pythonNetwork1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-import requests
-import sys
-r = requests.get(sys.argv[1])
-if r.status_code >= 400:
-    print(f"Error code: {r.status_code}")
-else:
-    print(r.text)
+"""urllib module"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print(f"Error code: {r.status_code}")
+    else:
+        print(r.text)
