17262d846c165903b1af984d366b3323f5d39224
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-10-24 06:07:19 -0700

    tasks

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,7 @@
+#!/usr/bin/node
 const fs = require('fs');
 
-fs.readFile('/Users/Rahmah/ALX/alx-higher_level_programming/0x14-javascript-web_scraping/cisfun', 'utf8', (err, data) => {
+fs.readFile('process.argv[2]', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
