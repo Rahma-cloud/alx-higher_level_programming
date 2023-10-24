c3bac9b9c79fe282156754d01c6339aea1dda074
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-10-24 06:25:24 -0700

    tasks

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], (err, data) => {
+fs.writeFile(process.argv[2], process.argv[3], err => {
   if (err) {
     console.error(err);
-    return;
   }
-  console.log(data);
 });
